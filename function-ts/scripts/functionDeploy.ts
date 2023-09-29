9463fea90da5ab791f8ba9f80eb3564e048fbde7
Lou-Kamades <lou_kamades@proton.me>
2023-09-29 00:55:41 -0500

    can deploy function properly

--- function-ts/scripts/functionDeploy.ts
+++ function-ts/scripts/functionDeploy.ts
@@ -1,4 +1,4 @@
-import { Connection, Keypair } from "@solana/web3.js";
+import { Connection, Keypair, Transaction } from "@solana/web3.js";
 import { parseMrEnclave } from "@switchboard-xyz/common";
 import { SwitchboardProgram, SwitchboardWalletFundParams } from "@switchboard-xyz/solana.js";
 import {
@@ -11,12 +11,13 @@ async function main() {
 
     const RPC_URL=''
     const KEYPAIR_PATH=''
+    const connection = new Connection(RPC_URL)
 
     const secretKey = JSON.parse(fs.readFileSync(KEYPAIR_PATH, 'utf-8'))
     const kp = Keypair.fromSecretKey(Uint8Array.from(secretKey));
 
     const program = await SwitchboardProgram.load(
-        new Connection(RPC_URL),
+        connection,
         kp
         );
 
@@ -25,18 +26,33 @@ async function main() {
         "CkvizjVnm2zA5Wuwan34NhVT3zFc7vqUyGnA6tuEF5aE" // devnet attestation queue
     );
 
-    const [functionAccount] = await FunctionAccount.create(program, {
-        name: "UPDATE_ORACLE",
-        metadata: "updates the oracle counter",
-        schedule: "30 * * * * *", // every 30 seconds
-        container: "loukamades/poc-switchboard-oracle",
-        containerRegistry: "dockerhub",
-        version: "latest",
-        mrEnclave: parseMrEnclave("0xc975678a533fb8cfe8b790c96d400fdecaf3f2bb580abf32d849909122ccdabc"), // run 'make measurement' and pass in here
-        attestationQueue: attestationQueueAccount,
-    });
+    // const [functionAccount] = await FunctionAccount.create(program, {
+    //     name: "RUST_ORACLE2",
+    //     metadata: "updates the oracle counter",
+    //     schedule: "30 * * * * *", // every 30 seconds
+    //     container: "loukamades/poc-switchboard-oracle",
+    //     containerRegistry: "dockerhub",
+    //     version: "latest",
+    //     mrEnclave: parseMrEnclave("0x58da7605bd9e55284fb3a2024c5cfff07b405391d5e819b584869e8d959e1e7c"), // run 'make measurement' and pass in here
+    //     attestationQueue: attestationQueueAccount,
+    // });
 
-    console.log(functionAccount.publicKey.toBase58())
+    const [functionAccount, functionInit] =
+    await attestationQueueAccount.createFunctionInstruction(kp.publicKey, {
+      name: "rust_Ix_test",
+      schedule: "15 * * * * *",
+      container: "loukamades/poc-switchboard-oracle",
+      containerRegistry: "dockerhub",
+      version: `latest`,
+      mrEnclave: parseMrEnclave("0xa814a864bf5aac27751ae2c60fdece42f570424dbde384e171395633502bdfe9")
+    });
+    console.log(`SWITCHBOARD_FUNCTION: ${functionAccount.publicKey}`);
+
+    const txn = new Transaction()
+    txn.add(...functionInit.ixns)
+    const signature = await connection.sendTransaction(txn, [kp])
+
+    console.log(signature)
 
 }
 
